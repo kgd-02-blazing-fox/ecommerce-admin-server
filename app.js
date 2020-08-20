2aab6156732989654e01b77c0bb2ad26dfca56fa
arissatya <arissatya.nugraha@gmail.com>
2020-08-20 22:39:00 +0700

    socket io applied

--- app.js
+++ app.js
@@ -6,15 +6,25 @@ if(process.env.NODE_ENV === 'development'){
 
 const express = require('express');
 const app = express()
+
 const cors = require('cors')
+// const whitelist = ['http://localhost:8080'];
+// const corsOptions = {
+//   credentials: true, // This is important.
+//   origin: (origin, callback) => {
+//     if(whitelist.includes(origin))
+//       return callback(null, true)
+//       callback(new Error('Not allowed by CORS'));
+//   }
+// }
 
 const index = require('./routes/index')
 const errorHandler = require('./middlewares/errorHandler')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+// app.use(cors(corsOptions));
 app.use(cors())
-
 app.use('/', index)
 app.use(errorHandler)
 
--- bin/http.js
+++ bin/http.js
@@ -1,7 +1,17 @@
 const app = require('../app.js')
 
-const PORT = process.env.PORT || 3000
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 
-app.listen(PORT, ()=>{
-  console.log('ecommerce server is online!');
+io.on('connect', function(socket){
+  console.log('CLIENT CONNECTED');
+
+  socket.on('newproduct', function(){
+    socket.broadcast.emit('CHANGE')
+  })
+})
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, ()=>{
+  console.log('ecommerce server is online ' + PORT);
 })
